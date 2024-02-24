1f8bfc0a766a9f46303e40edac28f119e8fc82af
Kevin Do <91227948+KevintrDo@users.noreply.github.com>
2024-02-23 23:16:34 -0500

    Fixed Cors

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 // app.js
-
+const cors = require('cors');
 const express = require('express');
 const bodyParser = require('body-parser');
 const authRoutes = require('./Routes/userRoutes');
 const adminRoutes = require('./Routes/adminRoutes');
 const appRoutes = require('./Routes/appRoute');
-const cors = require('cors');
 
 const app = express();
 const port = 3000;
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(authRoutes);
 app.use(adminRoutes);
 app.use(appRoutes);
-app.use(cors());
 
 // Increase payload size limit to 10MB (adjust as needed)
 app.use(bodyParser.json({ limit: '10mb' }));
@@ -23,5 +22,5 @@ app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log('Server is running on http://localhost:${port}');
 });
