b8906bfb9c6a8546d7939bf31583b1e3106b599f
danielgrigsby03 <daniel.grigsby.03@gmail.com>
2024-03-18 00:07:51 -0400

    Fixed localhost:3001

--- controllers/userController.js
+++ controllers/userController.js
@@ -296,7 +296,7 @@ const logout = (req, res) => {
               subject: 'Password Reset',
               html: `<p>You are receiving this email because you (or someone else) have requested the reset of the password for your account.</p>
                      <p>Please click on the following link, or paste this into your browser to complete the process:</p>
-                     <p><a href="http://localhost:3000/reset/${token}">Reset Password</a></p>
+                     <p><a href="http://localhost:3001/reset/${token}">Reset Password</a></p>
                      <p>If you did not request this, please ignore this email and your password will remain unchanged.</p>`,
           };
           console.log(token)
